1a56d2d497bd7ed21254fc82a4125accc901b461
Lava Kumar Gowda M A <91609093+nameisluv@users.noreply.github.com>
2022-10-28 00:27:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbarr").style.top = "0";
-  } else {
-    document.getElementById("navbarr").style.top = "-10rem";
+var a=2;
+function display() {
+    console.log(a)
+    if(a%2==0){
+    var ele = document.getElementById("zzx");
+    ele.style.left="0"
+    }
+    else
+    var left = document.getElementById("zzx").style.left="-3rem";
+    a++;
   }
-  prevScrollpos = currentScrollPos;
-}
